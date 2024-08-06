b980ae45246cb10b873783328cac5229c122b0d2
Derek <derek@meer.email>
2024-08-06 15:17:57 -0700

    add ReelName tag

--- lib/exexif/decode.ex
+++ lib/exexif/decode.ex
@@ -101,6 +101,7 @@ defmodule Exexif.Decode do
   def tag(_, 0xA434, value), do: {:lens_model, value}
   def tag(_, 0xA435, value), do: {:lens_serial_number, value}
   def tag(_, 0xA436, value), do: {:title, value}
+  def tag(_, 0xC789, value), do: {:reel_name, value}
   def tag(_, 0xFDEA, value), do: {:lens, value}
 
   # http://www.sno.phy.queensu.ca/~phil/exiftool/TagNames/GPS.html
